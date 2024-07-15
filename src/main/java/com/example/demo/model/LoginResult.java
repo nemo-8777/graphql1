4ab47a3bd86329067454474cdfe8c23717642884
nemo-8777 <ny121214@icloud.com>
2024-07-15 23:36:50 +0900

    second

--- src/main/java/com/example/demo/model/LoginResult.java
+++ src/main/java/com/example/demo/model/LoginResult.java
@@ -1,11 +1,8 @@
 package com.example.demo.model;
 
-import org.springframework.stereotype.Component;
-
 import lombok.Data;
 
 @Data
-@Component
 public class LoginResult {
 	private int resultcode;
 	private String message;
--- src/main/java/com/example/demo/resolver/LoginResolverImpl.java
+++ src/main/java/com/example/demo/resolver/LoginResolverImpl.java
@@ -1,6 +1,5 @@
 package com.example.demo.resolver;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
@@ -8,11 +7,9 @@ import com.example.demo.model.LoginResult;
 
 @Component
 public class LoginResolverImpl implements GraphQLQueryResolver{
-	
-	@Autowired
-	LoginResult loginResult;
-	
 	public LoginResult login(String username, String password) {
+		LoginResult loginResult = new LoginResult();
+		
 		if(username == "username" && password == "password") {
 			loginResult.setResultcode(1);
 			loginResult.setMessage("success");
